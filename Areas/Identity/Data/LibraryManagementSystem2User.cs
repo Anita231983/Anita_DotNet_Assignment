26fd6b5dd16959ecd2e332dc1d1bc5555676e5e7
acer <acer@ANITAPC>
2025-02-14 13:03:33 +0545

    Add project files.

---
+++ Areas/Identity/Data/LibraryManagementSystem2User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LibraryManagementSystem2.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the LibraryManagementSystem2User class
+public class LibraryManagementSystem2User : IdentityUser
+{
+}
+
